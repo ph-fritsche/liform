f90d0efd453ac7e2509f9ae90a1a7c02637d581d
Philipp Fritsche <ph.fritsche@nextdev.eu>
2020-05-24 12:09:21 +0200

    Add translated placeholder

--- src/Extension/AttrExtension.php
+++ src/Extension/AttrExtension.php
@@ -13,16 +13,37 @@ namespace Pitch\Liform\Extension;
 
 use Pitch\Liform\TransformResult;
 use Symfony\Component\Form\FormView;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * Extracts the attr from FormViews.
  */
 class AttrExtension implements ExtensionInterface
 {
+    protected ?TranslatorInterface $translator;
+
+    public function __construct(
+        TranslatorInterface $translator = null
+    ) {
+        $this->translator = $translator;
+    }
+
     public function apply(
         TransformResult $transformResult,
         FormView $formView
     ) {
         $transformResult->schema->attr = $formView->vars['attr'];
+
+        $placeholder = $formView->vars['attr']['placeholder'] ?? null;
+        if (isset($placeholder)) {
+            if (isset($this->translator)) {
+                $placeholder = $this->translator->trans(
+                    $placeholder,
+                    $formView->vars['attr_translation_parameters'],
+                    $formView->vars['translation_domain'],
+                );
+            }
+            $transformResult->schema->placeholder = $placeholder;
+        }
     }
 }
